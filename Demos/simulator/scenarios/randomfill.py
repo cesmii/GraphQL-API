944f9eb56ff7510575caa7cc8f0360479d39653b
David Wickman <david.wickman@cesmii.org>
2021-05-14 14:36:28 -0400

    ✨ add simulate random fill speeds definition

--- Demos/simulator/scenarios/randomfill.py
+++ Demos/simulator/scenarios/randomfill.py
@@ -1,9 +1,67 @@
 from utils import *
 
 import paho.mqtt.client as mqtt
+import random
 import time
 
+# max_tank_volume = 100
+change_interval = 5
+interval_count = 0
+current_flow_rate = 1
+tank_volume = 0
+
+def change_flow_rate():
+    global current_flow_rate
+    current_flow_rate = random.randint(1, 10)
+
+def fill_tank(topic, mqtt_client, flow_rate):
+    global interval_count
+    global tank_volume
+
+    tank_volume += flow_rate
+    set_interval_count()
+    mqtt_publish(str(tank_volume), topic, mqtt_client)
+
+    print("flow_rate: " + str(flow_rate))
+    time.sleep(1)
+
+def set_interval_count(): 
+    global change_interval
+    global interval_count
+
+    if interval_count == change_interval:
+        change_flow_rate()
+        interval_count = 0
+
+    interval_count += 1
+
 def simulate_randomfill(randomtanks, count_fill, lines, topic, mqtt_client):
+    """Simulate random fill speeds
+
+    [description]
+
+    TODO: 
+        - set a limit on the max fill
+        - use n tanks
+    
+    Arguments:
+        randomtanks {int} -- [description]
+        count_fill {int} -- number of tanks
+        lines {list} -- lines in the .txt file
+        topic {str} -- mqtt topic name
+        mqtt_client {class} -- mqtt class
+    """
+
+    try:
+        while True:
+            fill_tank(topic, mqtt_client, current_flow_rate);
+
+    except KeyboardInterrupt:
+        print()
+        print("Simulation stopped")
+        exit()
+
+def simulate_randomfill_original(randomtanks, count_fill, lines, topic, mqtt_client):
     try:
         while True:
             count = 0
