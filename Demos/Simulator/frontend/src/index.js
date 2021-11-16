9bc386e11a38455d7c0e52660a13bc26ac50b91b
wenjie <wenjie@WenjieMBP.lan>
2021-11-16 12:38:37 -0800

    ignore config

--- Demos/Simulator/frontend/src/index.js
+++ Demos/Simulator/frontend/src/index.js
@@ -10,12 +10,9 @@ import "assets/styles/tailwind.css";
 // layouts
 
 import Admin from "layouts/Admin.js";
-import Auth from "layouts/Auth.js";
 
 // views without layouts
 
-import Landing from "views/Landing.js";
-import Profile from "views/Profile.js";
 import Index from "views/Index.js";
 
 const instanceGraphQLEndpoint = "https://sandbox.cesmii.net/graphql";
@@ -258,10 +255,7 @@ ReactDOM.render(
       <Switch>
         {/* add routes with layouts */}
         <Route path="/admin" component={Admin} />
-        <Route path="/auth" component={Auth} />
         {/* add routes without layouts */}
-        <Route path="/landing" exact component={Landing} />
-        <Route path="/profile" exact component={Profile} />
         <Route path="/" exact component={Index} />
         {/* add redirect for first page */}
         <Redirect from="*" to="/" />
