bb4d13d36b4350683d41ea7269c407966c0d7e8e
jwise-cesmii <jonathan.wise@cesmii.org>
2023-06-23 11:03:03 -0400

    add CNC simulator demo

---
+++ Demos/CNC Simulator/config-example.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+smip = {
+    "authenticator": "authenticator name",
+    "password": "authenticator password",
+    "name": "username",
+    "role": "ROLE",
+    "url": "https://sandbox.cesmii.net/graphql",
+    "verbose": True
+}
