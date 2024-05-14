a22599e1eb98bc2a084696e35e9dec73c8d52d0d
jwise-mfg <info@jonathanwise.net>
2024-05-14 16:00:55 -0400

    move to new opcua library

--- Demos/Simple OPCUA Server/OPCUAServer.py
+++ Demos/Simple OPCUA Server/OPCUAServer.py
@@ -1,79 +1,74 @@
 #! /usr/bin/env python3
 
 ''' Dependenices to install via pip:
-      pip install pyspectator
-      pip install opcua 
-    Note: This generally works on Windows (including WSL) with some warnings, however pyspectator requires VisualStudio C++ Build tools:
-      - Microsoft C++ 14 Build Tools https://visualstudio.microsoft.com/visual-cpp-build-tools/
-      - Make sure you check the box for C++ Desktop development
-    Windows also requires an additional Python package:
-      pip install wmi
-    Starting with Python3.10, pyspectator doesn't work without modification:
-      - in collections.py, changed line 1 to: from collections.abc import MutableMapping, Container
-      - in processor.py, replaced line 5 with two lines:
-            from collections.abc import Mapping
-            from collections.abc import Callable
+      pip install asyncua
 '''
 
-from opcua import Server
-from pyspectator.processor import Cpu
-from pyspectator.computer import Computer
-from random import randint
+import asyncio
+from asyncua import ua, Server
+import random
 import datetime
 import time
 
-server_url = "opc.tcp://localhost:62548"
+server_url = "opc.tcp://localhost:62549/simple"
 server_name = "SIMPLE_OPCUA_SERVER"
 sample_rate = 2
 
-print("Configuring Simple OPC UA Server...")
-my_ua_server = Server()
-my_ua_server.set_endpoint(server_url)
+async def main():
+      print("Configuring Simple OPC UA Server...")
+      my_ua_server = Server()
+      await my_ua_server.init()
+      my_ua_server.set_endpoint(server_url)
 
-# Register a new namespace to work in
-my_ua_namespace = my_ua_server.register_namespace(server_name)
-print("Namespace ID: {}".format(my_ua_namespace))
+      # Register a new namespace to work in
+      my_ua_namespace = await my_ua_server.register_namespace(server_name)   
+      print("Namespace ID: {}".format(my_ua_namespace))
 
-# Find the "Objects" folder to attach a new object to
-my_ua_node = my_ua_server.get_objects_node()
-print("Objects Node ID: ", my_ua_node)
+      # Find the "Objects" folder to attach a new object to
+      my_ua_node = my_ua_server.get_objects_node()
+      print("Objects Node ID: ", my_ua_node)
 
-# Create a new object
-my_object = my_ua_node.add_object(my_ua_namespace, "MyObject")
-print("MyObject ID:", my_object)
+      # Create a new ObjectType and corresponding Object instance
+      mycustomobj_type = await my_ua_server.nodes.base_object_type.add_object_type(my_ua_namespace, "MyCustomObjectType")
+      check_var = await mycustomobj_type.add_variable(0, "var_should_be_there_after_instantiate", 1.0)  # demonstrates instantiate
+      await check_var.set_modelling_rule(True)  # make sure the variable is instansiated
+      my_object = await my_ua_server.nodes.objects.add_object(my_ua_namespace, "MyCustomObjectA", mycustomobj_type.nodeid)
+      print("MyObject ID:", my_object)
 
-# Add some attributes (of type variable) to MyObject
-#     Make sure the variable value is of the datatype you plan to send later
-attrib_load = my_object.add_variable(my_ua_namespace, "CPU Load", 0.0)  #0.0 since this will be a float
-attrib_press = my_object.add_variable(my_ua_namespace, "Memory Pressure", 0)  #0 since this will be an int
-attrib_time = my_object.add_variable(my_ua_namespace, "Time", datetime.datetime.now())
-# This attribute value can be written to by a client
-attrib_toggler = my_object.add_variable(my_ua_namespace, "Toggle Bit", False) #false since this will be a bool
-attrib_toggler.set_writable()
+      # Add some attributes (of type variable) to MyObject
+      #     Make sure the variable value is of the datatype you plan to send later
+      attrib_rnd = await my_object.add_variable(my_ua_namespace, "Random Number", 0)  #0 since this will be an int
+      attrib_time = await my_object.add_variable(my_ua_namespace, "Time", datetime.datetime.now())
+      # This attribute value can be written to by a client
+      attrib_toggler = await my_object.add_variable(my_ua_namespace, "Toggle Bit", False) #false since this will be a bool
+      await attrib_toggler.set_writable()
+      print("Added variables!")
 
-try:
-    print()
-    print("Starting Simple OPC UA Server...")
-    my_ua_server.start()
-    print()
-    print("Generating Sample Data...")
+      try:
+            print()
+            print("Starting Simple OPC UA Server...")
+            #await my_ua_server.start()
+            print()
+            print("Generating Sample Data...")
 
-    while True:
-          #Update the attribute values for my object
-          cpu = Cpu(monitoring_latency=1)
-          computer = Computer()
+            async with my_ua_server:
+                  while True:
+                        #Update the attribute values for my object
+                        new_rnd = random.randint(0,99)
+                        new_time = datetime.datetime.now()
+                        new_toggle = await attrib_toggler.get_value()
+                        await attrib_rnd.set_value(new_rnd)
+                        await attrib_time.set_value(new_time)
 
-          attrib_load.set_value(cpu.load)
-          attrib_press.set_value(computer.virtual_memory.used_percent)
-          attrib_time.set_value(datetime.datetime.now())
+                        print ("Current values: ", new_rnd, new_time, new_toggle)
 
-          print ("Current values: ", attrib_load.get_value(), attrib_press.get_value(), attrib_time.get_value(), attrib_toggler.get_value())
+                        await asyncio.sleep(sample_rate)
+      finally:
+            my_ua_server.stop()
+            print("Server Offline")
 
-          time.sleep(sample_rate)
-finally:
-    my_ua_server.stop()
-    print("Server Offline")
-
+if __name__ == "__main__":
+    asyncio.run(main())
 
 
 
