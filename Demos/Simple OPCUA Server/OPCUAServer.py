ca7777a87e15085bc290bd83688f16ae4275d0b7
jwise-mfg <info@jonathanwise.net>
2024-05-15 14:18:00 -0400

    add some machinery building block elements

--- Demos/Simple OPCUA Server/OPCUAServer.py
+++ Demos/Simple OPCUA Server/OPCUAServer.py
@@ -10,7 +10,7 @@ import random
 import datetime
 import time
 
-server_url = "opc.tcp://localhost:62549/simple"
+server_url = "opc.tcp://localhost:51210/example"
 server_name = "SIMPLE_OPCUA_SERVER"
 sample_rate = 2
 
@@ -29,19 +29,29 @@ async def main():
       print("Objects Node ID: ", my_ua_node)
 
       # Create a new ObjectType and corresponding Object instance
-      mycustomobj_type = await my_ua_server.nodes.base_object_type.add_object_type(my_ua_namespace, "MyCustomObjectType")
-      check_var = await mycustomobj_type.add_variable(0, "var_should_be_there_after_instantiate", 1.0)  # demonstrates instantiate
-      await check_var.set_modelling_rule(True)  # make sure the variable is instansiated
-      my_object = await my_ua_server.nodes.objects.add_object(my_ua_namespace, "MyCustomObjectA", mycustomobj_type.nodeid)
-      print("MyObject ID:", my_object)
+      equipment_type = await my_ua_server.nodes.base_object_type.add_object_type(my_ua_namespace, "EquipmentType")
+      machineid_type = await my_ua_server.nodes.base_object_type.add_object_type(my_ua_namespace, "MachineIdentificationType")
+      my_machine = await my_ua_server.nodes.objects.add_object(my_ua_namespace, "My Machine", equipment_type.nodeid)
+      my_machine_id = await my_machine.add_object(my_ua_namespace, "Machine Identification", machineid_type.nodeid)
+      
+      print("My Machine:", my_machine)
 
-      # Add some attributes (of type variable) to MyObject
+      # Add some attributes (of type variable) to my_machine
       #     Make sure the variable value is of the datatype you plan to send later
-      attrib_rnd = await my_object.add_variable(my_ua_namespace, "Random Number", 0)  #0 since this will be an int
-      attrib_time = await my_object.add_variable(my_ua_namespace, "Time", datetime.datetime.now())
-      # This attribute value can be written to by a client
-      attrib_toggler = await my_object.add_variable(my_ua_namespace, "Toggle Bit", False) #false since this will be a bool
+      attrib_rnd = await my_machine.add_variable(my_ua_namespace, "Random Number", 0)  #0 since this will be an int
+      attrib_time = await my_machine.add_variable(my_ua_namespace, "Current Time", datetime.datetime.now()) #this will be a Time
+      #     This attribute value can be written to by a client
+      attrib_toggler = await my_machine.add_variable(my_ua_namespace, "Run Bit", False) #false since this will be a bool
       await attrib_toggler.set_writable()
+
+      # Add some attributes to my_machine_id, since these are static, we won't assign a variable
+      await my_machine_id.add_variable(my_ua_namespace, "Manufacturer", "CESMII")  #this will be a string, per the spec
+      await my_machine_id.add_variable(my_ua_namespace, "ManufacturerUri", "https://www.cesmii.org")  #this will be a string, per the spec
+      await my_machine_id.add_variable(my_ua_namespace, "MonthOfConstruction", "5")  #this will be an int, per the spec
+      await my_machine_id.add_variable(my_ua_namespace, "InitialOperationDate", datetime.datetime.now())  #this will be a Time, per the spec
+      attrib_location = await my_machine_id.add_variable(my_ua_namespace, "Location", "Los Angeles, CA")  #this will be writeable and a string, per the spec
+      await attrib_location.set_writable()
+
       print("Added variables!")
 
       try:
@@ -69,7 +79,3 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
-
-
-
-
